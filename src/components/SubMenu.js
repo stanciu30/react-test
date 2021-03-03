dc09b7df7a4658cc81d8e36f07caf170c935bbfb
stanciu30 <stanciu30@yahoo.com>
2021-03-03 23:11:12 +0200

    1 file modified

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -5,9 +5,23 @@ import {Link} from 'react-router-dom';
 const SidebarLink = styled(Link)`
   display: flex;
   color: #e1e9fc;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+
+  &:hover{
+    background: #252831;
+    border-left: 4px solid #632ce4;
+    cursor: pointer;
+  }
 `;
 
 const SidebarLabel = styled.span`
+  margin-left: 16px;
 
 `;
 
