3397c251d6ff1b0a18ab857172350681e5d0caa4
stanciu30 <stanciu30@yahoo.com>
2021-03-03 22:23:07 +0200

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./style.css";
+import Sidebar from './components/Sidebar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+//import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen </p>
-    </div>
+     <Router>
+    <Sidebar />
+     </Router>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,39 @@
+import React  from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+const Nav = styled.div`
+  background: #15171c;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171c;
+`;
+
+const Sidebar = () => {
+  return (
+    <>
+      <Nav>
+        <NavIcon to="#">
+          <FaIcons.FaBars />
+        </NavIcon>
+      </Nav>
+    </>
+  )
+}
+export default Sidebar;
